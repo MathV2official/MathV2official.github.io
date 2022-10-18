5cd880bde2a30d51591146c24ed5a00561b7514f
coolguy564738 <115120540+coolguy564738@users.noreply.github.com>
2022-10-17 21:48:19 -0500

    Update contents.js

--- contents.js
+++ contents.js
@@ -3,7 +3,712 @@ const contents = {
 	// url = cross-origin resource (with proxy)
 	// TODO add preview images for every game
   
-  {
+  	flashGames: [
+		{
+			name: "3 Foot Ninja",
+			path: "embed.html?type=flash&src=%2Fflash-games%2F3-foot-ninja.swf"
+		},
+		{
+			name: "3 Foot Ninja II",
+			path: "embed.html?type=flash&src=%2Fflash-games%2F3-foot-ninja-ii.swf"
+		},
+		{
+			name: "3D Reversi",
+			path: "embed.html?type=flash&src=%2Fflash-games%2F3d-reversi.swf"
+		},
+		{
+			name: "625 Sandwich Stacker",
+			path: "embed.html?type=flash&src=%2Fflash-games%2F625-sandwich-stacker.swf"
+		},
+		{
+			name: "7 Days Without Rain",
+			path: "embed.html?type=flash&src=%2Fflash-games%2F7-days-without-rain.swf"
+		},
+		{
+			name: "Ace of Space",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Face-of-space.swf"
+		},
+		{
+			name: "Achivement Unlocked",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fachivement-unlocked.swf"
+		},
+		{
+			name: "Action Driving Game",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Faction-driving-game.swf"
+		},
+		{
+			name: "Addiction Solitaire",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Faddiction-solitaire.swf"
+		},
+		{
+			name: "Age of War",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fage-of-war.swf"
+		},
+		{
+			name: "Alien Hominid",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Falien-hominid.swf"
+		},
+		{
+			name: "Amorphous Plus",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Famorphous-plus.swf"
+		},
+		{
+			name: "Asteroids",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fasteroids.swf"
+		},
+		{
+			name: "Astroflash",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fastroflash.swf"
+		},
+		{
+			name: "Atari Breakout",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fatari-breakout.swf"
+		},
+		{
+			name: "Avalanche",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Favalanche.swf"
+		},
+		{
+			name: "Battle Pong",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fbattle-pong.swf"
+		},
+		{
+			name: "Battleships",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fbattleships.swf"
+		},
+		{
+			name: "Bloons Player Pack 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fbloons-player-pack-2.swf"
+		},
+		{
+			name: "Bloons Player Pack 3",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fbloons-player-pack-3.swf"
+		},
+		{
+			name: "Bloons Player Pack 4",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fbloons-player-pack-4.swf"
+		},
+		{
+			name: "Bloons Tower Defense",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fbloons-tower-defense.swf"
+		},
+		{
+			name: "Bloons Tower Defense 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fbloons-tower-defense-2.swf"
+		},
+		{
+			name: "Bloxorz",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fbloxorz.swf"
+		},
+		{
+			name: "Bowman",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fbowman.swf"
+		},
+		{
+			name: "Boxhead 2 Play",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fboxhead-2-play.swf"
+		},
+		{
+			name: "Breaking The Bank",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fbreaking-the-bank.swf"
+		},
+		{
+			name: "Bubble Shooter",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fbubble-shooter.swf"
+		},
+		{
+			name: "Bubble Tanks 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fbubble-tanks-2.swf"
+		},
+		{
+			name: "Bubble Trouble",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fbubble-trouble.swf"
+		},
+		{
+			name: "Bullet Bill",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fbullet-bill.swf"
+		},
+		{
+			name: "Castle Wars",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fcastle-wars.swf"
+		},
+		{
+			name: "Causality",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fcausality.swf"
+		},
+		{
+			name: "Champion Archer",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fchampion-archer.swf"
+		},
+		{
+			name: "Choose Your Weapon",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fchoose-your-weapon.swf"
+		},
+		{
+			name: "Choose Your Weapon 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fchoose-your-weapon-2.swf"
+		},
+		{
+			name: "Choose Your Weapon 3",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fchoose-your-weapon-3.swf"
+		},
+		{
+			name: "Choose Your Weapon 4",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fchoose-your-weapon-4.swf"
+		},
+		{
+			name: "Color Switch",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fcolor-switch.swf"
+		},
+		{
+			name: "Connect 4",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fconnect-4.swf"
+		},
+		{
+			name: "Connect the World",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fconnect-the-world.swf"
+		},
+		{
+			name: "Conquer Antarctica",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fconquer-antarctica.swf"
+		},
+		{
+			name: "Crimson Room",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fcrimson-room.swf"
+		},
+		{
+			name: "Cubefield",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fcubefield.swf"
+		},
+		{
+			name: "Cursor 10",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fcursor-10.swf"
+		},
+		{
+			name: "Cursor Invisible",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fcursor-invisible.swf"
+		},
+		{
+			name: "Curveball",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fcurveball.swf"
+		},
+		{
+			name: "Dr. Carter and the Cave of Despair",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fdr-carter-and-the-cave-of-despair.swf"
+		},
+		{
+			name: "Duck Life",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fduck-life.swf"
+		},
+		{
+			name: "Duck Life 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fduck-life-2.swf"
+		},
+		{
+			name: "Duck Life 3",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fduck-life-3.swf"
+		},
+		{
+			name: "Duck Life 4",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fduck-life-4.swf"
+		},
+		{
+			name: "Effing Worms",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Feffing-worms.swf"
+		},
+		{
+			name: "Eggtastic",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Feggtastic.swf"
+		},
+		{
+			name: "Electric Box",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Felectric-box.swf"
+		},
+		{
+			name: "Electric Man 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Felectric-man-2.swf"
+		},
+		{
+			name: "Escaping The Prison",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fescaping-the-prison.swf"
+		},
+		{
+			name: "Extinct",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fextinct.swf"
+		},
+		{
+			name: "Falldown",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Ffalldown.swf"
+		},
+		{
+			name: "Falldown 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Ffalldown2.swf"
+		},
+		{
+			name: "Fancy Pants Adventure",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Ffancy-pants-adventure.swf"
+		},
+		{
+			name: "Fancy Pants Adventure 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Ffancy-pants-adventure2.swf"
+		},
+		{
+			name: "Fat Ninja",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Ffat-ninja.swf"
+		},
+		{
+			name: "Feed Us",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Ffeed-us.swf"
+		},
+		{
+			name: "Feudalism",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Ffeudalism.swf"
+		},
+		{
+			name: "Frogger",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Ffrogger.swf"
+		},
+		{
+			name: "Galaga",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fgalaga.swf"
+		},
+		{
+			name: "Gold Miner",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fgold_miner_xploit_machine_edition_2009.swf"
+		},
+		{
+			name: "Gold Miner (Special Edition)",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fgold_miner_special_edition.swf"
+		},
+		{
+			name: "Gridlock",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fgridlock.swf"
+		},
+		{
+			name: "Gun Mayhem 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fgun-mayhem-2.swf"
+		},
+		{
+			name: "Gun Mayhem 3",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fgun-mayhem-3.swf"
+		},
+		{
+			name: "Hobo",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fhobo.swf"
+		},
+		{
+			name: "Hobo 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fhobo-2.swf"
+		},
+		{
+			name: "Hobo 3",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fhobo-3.swf"
+		},
+		{
+			name: "Hobo 4",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fhobo-4.swf"
+		},
+		{
+			name: "Hobo 5",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fhobo-5.swf"
+		},
+		{
+			name: "Hobo 6",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fhobo-6.swf"
+		},
+		{
+			name: "Hobo 7",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fhobo-7.swf"
+		},
+		{
+			name: "Learn to Fly",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Flearn-to-fly.swf"
+		},
+		{
+			name: "Learn to Fly 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Flearn-to-fly-2.swf"
+		},
+		{
+			name: "Line Rider",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fline-rider.swf"
+		},
+		{
+			name: "Mindfields 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fmindfields-2.swf"
+		},
+		{
 			name: "Minecraft Tower Defence 2",
 			path: "embed.html?type=flash&src=%2Fflash-games%2Fminecraft-tower-defence-2.swf"
 		},
+		{
+			name: "Missile Strike",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fmissile-strike.swf"
+		},
+		{
+			name: "Monster Brawl",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fmonster-brawl.swf"
+		},
+		{
+			name: "Moss",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fmoss.swf"
+		},
+		{
+			name: "Multitask",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fmultitask.swf"
+		},
+		{
+			name: "N Game",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fn-game.swf"
+		},
+		{
+			name: "Pac Man",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fpac-man.swf"
+		},
+		{
+			name: "Pacman Advanced",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fpacman-advanced.swf"
+		},
+		{
+			name: "Pandemic 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fpandemic-2.swf"
+		},
+		{
+			name: "Papa's Pizzeria",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fpapas-pizzeria.swf"
+		},
+		{
+			name: "Penguin",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fpenguin.swf"
+		},
+		{
+			name: "Penguin Diner",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fpenguin-diner.swf"
+		},
+		{
+			name: "Plant VS Zombies 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fplant_vs_zombies_2.swf"
+		},
+		{
+			name: "Playing With Fire 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fplaying-with-fire-2.swf"
+		},
+		{
+			name: "Plazma Burst",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fplazma-burst.swf"
+		},
+		{
+			name: "Plumber 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fplumber-2.swf"
+		},
+		{
+			name: "Portal",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fportal.swf"
+		},
+		{
+			name: "Portal 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fportal-2.swf"
+		},
+		{
+			name: "Potty Racers",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fpotty-racers.swf"
+		},
+		{
+			name: "Potty Racers 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fpotty-racers-2.swf"
+		},
+		{
+			name: "Primary",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fprimary.swf"
+		},
+		{
+			name: "Puzzle Bobble",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fpuzzle-bobble.swf"
+		},
+		{
+			name: "Raft Wars",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fraft-wars.swf"
+		},
+		{
+			name: "Raft Wars 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fraft-wars-2.swf"
+		},
+		{
+			name: "Ragdoll Avalanche",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fragdoll-avalanche.swf"
+		},
+		{
+			name: "Ragdoll Avalanche 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fragdoll-avalanche-2.swf"
+		},
+		{
+			name: "Rage",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Frage.swf"
+		},
+		{
+			name: "Rage 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Frage-2.swf"
+		},
+		{
+			name: "Rage 3",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Frage-3.swf"
+		},
+		{
+			name: "Redbeard",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fredbeard.swf"
+		},
+		{
+			name: "Riddle School",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Friddle-school.swf"
+		},
+		{
+			name: "Riddle School 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Friddle-school-2.swf"
+		},
+		{
+			name: "Riddle School 3",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Friddle-school-3.swf"
+		},
+		{
+			name: "Riddle School 4",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Friddle-school-4.swf"
+		},
+		{
+			name: "Riddle School 5",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Friddle-school-5.swf"
+		},
+		{
+			name: "Riddle Transfer",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Friddle-transfer.swf"
+		},
+		{
+			name: "Riddle Transfer 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Friddle-transfer-2.swf"
+		},
+		{
+			name: "Roadblock",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Froadblock.swf"
+		},
+		{
+			name: "Roadblock 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Froadblock-2.swf"
+		},
+		{
+			name: "Scary Maze Game",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fscary-maze-game.swf"
+		},
+		{
+			name: "Shanghai Dynasty",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fshanghai-dynasty.swf"
+		},
+		{
+			name: "Shift",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fshift.swf"
+		},
+		{
+			name: "Shift 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fshift-2.swf"
+		},
+		{
+			name: "Shift 3",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fshift-3.swf"
+		},
+		{
+			name: "Shift 4",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fshift-4.swf"
+		},
+		{
+			name: "Shopping Cart Hero",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fshopping-cart-hero.swf"
+		},
+		{
+			name: "Shuffle",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fshuffle.swf"
+		},
+		{
+			name: "Skywire",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fskywire.swf"
+		},
+		{
+			name: "Skywire 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fskywire-2.swf"
+		},
+		{
+			name: "Snake",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fsnake.swf"
+		},
+		{
+			name: "Snow Storm",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fsnow-storm.swf"
+		},
+		{
+			name: "Solitaire",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fsolitaire.swf"
+		},
+		{
+			name: "Sonic",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fsonic.swf"
+		},
+		{
+			name: "Space Invaders",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fspace-invaders.swf"
+		},
+		{
+			name: "Stick RPG",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fstick-rpg.swf"
+		},
+		{
+			name: "Stupidity Test",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fstupidity-test.swf"
+		},
+		{
+			name: "Super Mario 63",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fsuper-mario-63.swf"
+		},
+		{
+			name: "Super Mario Flash",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fsuper-mario-flash.swf"
+		},
+		{
+			name: "Super Mario Flash 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fsuper-mario-flash-2.swf"
+		},
+		{
+			name: "Super Mario Sunshine 64",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fsuper-mario-sunshine-64.swf"
+		},
+		{
+			name: "Super Smash Flash",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fsuper-smash-flash.swf"
+		},
+		{
+			name: "Super Smash Flash 2",
+			path: "embed.html?type=flash&src=https%3A%2F%2Fbare.whitespider.gq%2Flfs%2Fflash-games%2Fsuper-smash-flash-2.swf"
+		},
+		{
+			name: "Swords and Sandals",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fswords-and-sandals.swf"
+		},
+		{
+			name: "Swords and Sandals 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fswords-and-sandals-2.swf"
+		},
+		{
+			name: "Tank Trouble 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Ftank-trouble-2.swf"
+		},
+		{
+			name: "Tanks",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Ftanks.swf"
+		},
+		{
+			name: "Territory War",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fterritory-war.swf"
+		},
+		{
+			name: "Tetris",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Ftetris.swf"
+		},
+		{
+			name: "The Battle",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fthe-battle.swf"
+		},
+		{
+			name: "The Bright From The Screen",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fthe-bright-from-the-screen.swf"
+		},
+		{
+			name: "The Classroom",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fthe-classroom.swf"
+		},
+		{
+			name: "The Impossible Game Lite",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fthe-impossible-game-lite.swf"
+		},
+		{
+			name: "The Impossible Quiz",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fthe-impossible-quiz.swf"
+		},
+		{
+			name: "The Impossible Quiz 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fthe-impossible-quiz-2.swf"
+		},
+		{
+			name: "The Last Stand Union City",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fthe-last-stand-union-city.swf"
+		},
+		{
+			name: "Thing Thing",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fthing-thing.swf"
+		},
+		{
+			name: "Thing Thing 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fthing-thing-2.swf"
+		},
+		{
+			name: "Thing Thing 3",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fthing-thing-3.swf"
+		},
+		{
+			name: "Thing Thing 4",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fthing-thing-4.swf"
+		},
+		{
+			name: "Thing Thing Arena",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fthing-thing-arena.swf"
+		},
+		{
+			name: "Thing Thing Arena 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fthing-thing-arena-2.swf"
+		},
+		{
+			name: "This is the Only Level",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fthis-is-the-only-level.swf"
+		},
+		{
+			name: "Tic Tac Toe",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Ftic-tac-toe.swf"
+		},
+		{
+			name: "Travelogue Escape",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Ftravelogue-escape.swf"
+		},
+		{
+			name: "Ultimate Chess",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fultimate-chess.swf"
+		},
+		{
+			name: "Unfair Mario",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Funfair-mario.swf"
+		},
+		{
+			name: "Warp Shot",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fwarp-shot.swf"
+		},
+		{
+			name: "Whack Your PC",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fwhack-your-pc.swf"
+		},
+		{
+			name: "World's Easiest Game",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fworlds-easiest-game.swf"
+		},
+		{
+			name: "World's Hardest Game",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fworlds-hardest-game.swf"
+		},
+		{
+			name: "World's Hardest Game 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fworlds-hardest-game-2.swf"
+		},
+		{
+			name: "Yuletide Riptide",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fyuletide-riptide.swf"
+		},
+		{
+			name: "Zombocalypse",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fzombocalypse.swf"
+		}
+		],
+};
+
+export default contents;
