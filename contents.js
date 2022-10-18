e3b0d8c8758bfcfb97324319e89172acd66cddc9
coolguy564738 <115120540+coolguy564738@users.noreply.github.com>
2022-10-17 21:40:42 -0500

    Create contents.js

---
+++ contents.js
@@ -0,0 +1,9 @@
+const contents = {
+  // path = same-origin resource (no proxy)
+	// url = cross-origin resource (with proxy)
+	// TODO add preview images for every game
+  
+  {
+			name: "Minecraft Tower Defence 2",
+			path: "embed.html?type=flash&src=%2Fflash-games%2Fminecraft-tower-defence-2.swf"
+		},
