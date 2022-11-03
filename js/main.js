9da361e9575872ed30ee4905c1aadf2fca36b82c
ZombieDuck2309 <117324663+ZombieDuck2309@users.noreply.github.com>
2022-11-03 15:41:16 -0500

    fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-// Hello there!
-//
-// If you want to add my games to your site, please reach out at my email: echo-the-coder@tuta.io, or discord: 3kh0_#1791
-
 console.warn(
   "%cNote!",
   "color: purple; font-weight: 600; background: yellow; padding: 0 5px; border-radius: 5px",
