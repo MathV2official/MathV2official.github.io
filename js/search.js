4ac06d803bc35a2c9165286e6826fa19834b1089
ZombieDuck2309 <117324663+ZombieDuck2309@users.noreply.github.com>
2023-01-23 19:23:02 -0600

    wasd

--- js/search.js
+++ js/search.js
@@ -2,7 +2,7 @@ function startSearch() {
   var input, filter, ul, li, a, i;
   input = document.getElementById("mySearch");
   filter = input.value.toUpperCase();
-  ul = document.getElementById("search");
+  ul = document.getElementById("circle");
   li = ul.getElementsByTagName("a");
   for (i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName("h1")[0];
