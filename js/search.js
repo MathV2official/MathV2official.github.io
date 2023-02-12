74876d34ec8a6c1817f39afabf1ea98b1509150f
MathV2official <123830694+MathV2official@users.noreply.github.com>
2023-02-11 21:02:49 -0600

    Update search.js

--- js/search.js
+++ js/search.js
@@ -2,7 +2,7 @@ function startSearch() {
   var input, filter, ul, li, a, i;
   input = document.getElementById("mySearch");
   filter = input.value.toUpperCase();
-  ul = document.getElementById("circle");
+  ul = document.getElementById("search");
   li = ul.getElementsByTagName("a");
   for (i = 0; i < li.length; i++) {
     a = li[i].getElementsByTagName("h1")[0];
