d265ecee9f5e9dbac31669ce3daafee8502fc663
MathV2official <123830694+MathV2official@users.noreply.github.com>
2023-02-23 08:43:04 -0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const circles = document.querySelectorAll('.circle');
-
-circles.forEach((circle) => {
-  circle.addEventListener('mouseover', () => {
-    circle.style.visibility = 'hidden';
-  });
-});
