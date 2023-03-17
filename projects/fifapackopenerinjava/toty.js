b2a95041f8fd0673ada146df66ac1590d778d0ff
MathV2official <123830694+MathV2official@users.noreply.github.com>
2023-03-17 15:16:45 -0500

    s

---
+++ projects/fifapackopenerinjava/toty.js
@@ -0,0 +1,75 @@
+var imagesFolder = "https://mathv22.netlify.app/Toty/"; // Path to the images folder
+var packImages = [
+  {name: "VanDijk", ovr: 96, image: "VanDijk.png"},
+  {name: "Hernandez", ovr: 94, image: "Hernandez.png"},
+  {name: "Mbappe", ovr: 97, image: "Mbappe.png"},
+  {name: "Messi", ovr: 98, image: "Messi.png"},
+  {name: "Haaland", ovr: 96, image: "Haaland.png"},
+  {name: "Benzema", ovr: 97, image: "Benzema.png"},
+  {name: "Hakimi", ovr: 94, image: "Hakimi.png"},
+  {name: "Courtois", ovr: 96, image: "Courtois.png"},
+  {name: "Modric", ovr: 96, image: "Modric.png"},
+  {name: "Bellingham", ovr: 95, image: "Bellingham.png"},
+  {name: "Eder", ovr: 94, image: "Eder.png"},
+  {name: "KDB", ovr: 97, image: "KDB.png"}
+]; // List of image filenames and their corresponding OVRs
+
+var packedImages = JSON.parse(getCookie("packedImages") || "[]");
+
+function displayPackedImages() {
+  var imageContainer = document.getElementById("imageContainer");
+  imageContainer.innerHTML = "";
+
+  for (var i = 0; i < packedImages.length; i++) {
+    var packedImage = packedImages[i];
+
+    // Create the image element
+    var image = document.createElement("img");
+    image.src = imagesFolder + packedImage.image;
+    image.id = packedImage.name;
+    imageContainer.appendChild(image);
+  }
+}
+
+// Function to open the pack
+function openTOTYPack() {
+  var imageContainer = document.getElementById("imageContainer");
+  imageContainer.innerHTML = "";
+
+  // Select a random pack image
+  var randomIndex = Math.floor(Math.random() * packImages.length);
+  var packImage = packImages[randomIndex];
+
+  // Create the image element
+  var image = document.createElement("img");
+  image.src = imagesFolder + packImage.image;
+  image.id = packImage.name;
+  imageContainer.appendChild(image);
+
+  // Create the counter element
+  var counter = document.createElement("div");
+  counter.id = "counter";
+  counter.innerText = "0";
+  imageContainer.appendChild(counter);
+
+  // Start the counter
+  var interval = setInterval(function() {
+    var currentCounter = parseInt(counter.innerText);
+    if (currentCounter < packImage.ovr) {
+      currentCounter++;
+      counter.innerText = currentCounter.toString();
+    } else {
+      clearInterval(interval);
+      // Add the packed image to the list
+      packedImages.push(packImage);
+      setCookie("packedImages", JSON.stringify(packedImages));
+    }
+  }, 10);
+}
+
+// Function to view the collection
+function viewCollection() {
+  window.location.href = "totycollection.html";
+}
+
+displayPackedImages();
